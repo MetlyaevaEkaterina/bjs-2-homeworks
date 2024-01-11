73c5cf9fd1fb0de87b9c211adc2c603b22637acc
Metlyaeva Ekaterina FJS-68 <ka.met357@gmail.com>
2024-01-11 17:36:47 +0700

    Первое и второе задания полностью готовы

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let dis = b**2 - 4 * a * c;
   let arr = [];
+
+  if (dis === 0) {
+    arr.push(-b / (2 * a))
+  } else if (dis > 0) {
+    arr.push((-b + Math.sqrt(dis)) / (2 * a));
+    arr.push((-b - Math.sqrt(dis)) / (2 * a))
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return "Есть не числовое выражение, исправьте"
+  } else {
+    let P = (percent / 100) / 12;
+    let n = countMonths;
+    let S = amount - contribution;
+
+    let monthPay = S * (P + (P / (((1 + P)**n) - 1)));
+    let sum = monthPay * countMonths;
+    return sum.toFixed(2)
+  }
 }
