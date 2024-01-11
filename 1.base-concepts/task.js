7ee055849f8f84014b6c62bf7f4cf75027999651
Metlyaeva Ekaterina FJS-68 <ka.met357@gmail.com>
2024-01-11 17:36:47 +0700

    Исправлены имена переменных и почищен код

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let discriminant = b**2 - 4 * a * c;
   let arr = [];
+
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a))
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a))
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyRate = (percent / 100) / 12;
+  let loanAmount = amount - contribution;
+
+  let monthPay = loanAmount * (monthlyRate + (monthlyRate / (((1 + monthlyRate)**countMonths) - 1)));
+  let sum = monthPay * countMonths;
+  return Number(sum.toFixed(2))  
 }
