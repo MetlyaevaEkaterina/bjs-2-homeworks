4a8dcd5b5f8fa1f1279d5083a04ea5327a49f8e7
Metlyaeva Ekaterina FJS-68 <ka.met357@gmail.com>
2024-01-29 22:38:03 +0700

    Первая попытка возможно не рабочее

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.hasOwnProperty("addMarks")) {
+    this.marksToAdd.push(...marksToAdd);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (marksToAdd.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < marksToAdd.length; i++) {
+        sum = sum + marksToAdd[i];
+    }
+    return sum / marksToAdd.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marksToAdd;
+
+  this.excluded = reason;
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
