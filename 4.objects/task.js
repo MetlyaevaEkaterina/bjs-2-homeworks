b0fcaafebb9b44d47b914b8957fa8b7357cb9912
Metlyaeva Ekaterina FJS-68 <ka.met357@gmail.com>
2024-01-30 16:47:11 +0700

    Проверка на готовность

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,6 +2,8 @@ function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
+
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -10,24 +12,24 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marksToAdd) {
   if (this.hasOwnProperty("addMarks")) {
-    this.marksToAdd.push(...marksToAdd);
+    this.marks.push(...marksToAdd);
   }
 }
 
 Student.prototype.getAverage = function () {
-    if (marksToAdd.length === 0) {
+    if (!this.hasOwnProperty("marks") || this.marks.length === 0) {
         return 0;
     }
-    let sum = 0;
-    for (let i = 0; i < marksToAdd.length; i++) {
-        sum = sum + marksToAdd[i];
+    this.sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+        this.sum = this.sum + this.marks[i];
     }
-    return sum / marksToAdd.length;
+    return this.sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
   delete this.subject;
-  delete this.marksToAdd;
+  delete this.marks;
 
   this.excluded = reason;
 }
