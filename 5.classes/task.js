94e6a815f0f47ccf2c6e162c6a4117a1b7040829
Metlyaeva Ekaterina FJS-68 <ka.met357@gmail.com>
2024-02-08 22:08:09 +0700

    still not ready

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -85,9 +85,9 @@ class PrintEditionItem {
     
     giveBookByName(bookName) {
       for (let i = 0; i < this.books.length; i++) {
-        if (this.books[i].name === bookName) {
+        if (this.books[i][name] === bookName) {
           this.books.splice(i, 1);
-          return this.books[i].name;
+          return this.books[i];
         }
       }
     }
