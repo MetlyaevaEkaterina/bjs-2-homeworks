f817e587e3fb5023c8c3e2dcf519952d9c2cb527
Metlyaeva Ekaterina FJS-68 <ka.met357@gmail.com>
2024-02-09 16:27:44 +0700

    ready

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -86,9 +86,10 @@ class PrintEditionItem {
     giveBookByName(bookName) {
       for (let i = 0; i < this.books.length; i++) {
         if (this.books[i].name === bookName) {
-          return this.books.splice(i, 1);
-          // return this.books[i]; // надо возвращать удаленный элемент, в books архиве его уже нет??
+          this.books.splice(i, 1);
+          return this.books.splice(i, 1)[0]; 
         }
       }
+      return null;
     }
   }
