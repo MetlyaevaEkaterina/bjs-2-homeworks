53054991e03e0d930b9e25b298a4ed82906629ea
Metlyaeva Ekaterina FJS-68 <ka.met357@gmail.com>
2024-03-02 17:19:43 +0700

    not ready

---
+++ 7.async/task.js
@@ -0,0 +1,57 @@
+class AlarmClock {
+    constructor () {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addclock (time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        } 
+
+        if (this.alarmCollection.includes(time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({callback, time, canCall: true});
+    }
+
+    removeClock (time) {
+        this.alarmCollection.filter((time) => this.time === time); // если не работает нужно добавить splice
+    }
+
+    getCurrentFormattedTime () {
+        this.currentDate = new Date();
+        return (this.currentDate.getHours() + ":" + this.currentDate.getMinutes())
+    }
+
+    start () {
+        let checkClock = (alarm) => {
+            if (alarm.time === getCurrentFormattedTime()) {
+              alarm.callback();
+              alarm.canCall = false;
+            }
+          }
+          if (this.timerId === null) {
+            this.timerId = setInterval(() => {
+              this.alarmCollection.forEach(alarm => checkClock(alarm));
+            }, 1000);
+          }
+    }
+
+    stop () {
+        if (this.timerId !== null) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+          }
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+
+    clearAlarms () {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
